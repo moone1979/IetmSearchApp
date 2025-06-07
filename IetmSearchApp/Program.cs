0bc480c847258740342454a79ace45f62234210c
moone1979 <moone1979@users.noreply.github.com>
2025-06-07 11:30:33 +0900

    Fix: Update Dockerfile and Program.cs

--- IetmSearchApp/Program.cs
+++ IetmSearchApp/Program.cs
@@ -1,5 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "80";
+builder.WebHost.UseUrls($"http://*:{port}");
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
@@ -13,7 +16,6 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
 app.UseRouting();
 
 app.UseAuthorization();
